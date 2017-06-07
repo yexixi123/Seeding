025b98053aed1f06da6df4dbe3adcb08c140ba94
yexixi123 <2357401088@qq.com>
2017-06-08 01:52:31 +0800

    集成页面，解决跨域请求问题

---
+++ src/main/java/com/sending/util/WebConfig.java
@@ -0,0 +1,17 @@
+/*package com.sending.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("*//**");
+        registry.addMapping("*//**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "TRACE");
+    }
+}*/
---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,8 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+$$('goreg').on('click',function(){
+    $$.ajax
+})
